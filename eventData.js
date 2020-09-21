d0eace5e5c323dbb044e24ff452077609e093a72
Jonathan Vadil <32116722+ImpulseGundam@users.noreply.github.com>
2020-09-21 07:44:51 -0700

    rev-9-17

---
+++ eventData.js
@@ -0,0 +1,8 @@
+events = [
+{"name": "Saturday Group Builds",
+"dates":["January 4th", "February 4th", "March 7th", "April 4th", "May 2nd", "June 6th", "July 4th", "August 4th", "September 5th", "October 3rd", "November 7th", "December 5th"]}
+{"name": "Stargazer Movie Nights",
+"dates":["March 28th", "June 27th", "September 26th", "December 26th"]}
+{"name": "Gunpla News",
+"dates":["Usually Mondays of the first week of the month."]}
+];
